5f55a1277ce5343bdea125aae2953b1e86098c0d
man1cato <andresrod21@gmail.com>
2018-08-29 16:33:32 -0400

    Testing API

--- server/server.js
+++ server/server.js
@@ -8,11 +8,16 @@ const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 
+const now = Date.now().toString();
+
+
 app.get('/', (req, res) => {
+    console.log(`${now}: GET /`);
     res.send('Automated jobs server is up');
 })
 
 app.post('/api', (req, res) => {
+    console.log(`${now}: POST /api`);
     console.log(req.body);
     res.send(req.body);
 })
